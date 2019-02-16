be6ef5ab40167e4b2d5808ccea875a51232ad9df
Kneesnap <davidmenlo2@gmail.com>
2019-02-16 14:03:31 -0800

    Create basic world configuration to load.

--- Client/src/hapless/eagles/client/Main.java
+++ Client/src/hapless/eagles/client/Main.java
@@ -2,6 +2,8 @@ package hapless.eagles.client;
 
 import hapless.eagles.common.World;
 import hapless.eagles.common.utils.Config;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.io.File;
 
@@ -9,7 +11,7 @@ import java.io.File;
  * The main client class.
  * Created by Kneesnap on 2/16/19.
  */
-public class Main {
+public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Client Start");
@@ -17,5 +19,12 @@ public class Main {
         World world = new World();
         world.load(new Config(new File("debug.cfg")));
         System.out.println("Loaded World Config.");
+
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
