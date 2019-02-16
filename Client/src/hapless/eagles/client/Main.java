b749baab07d27eae0913a7e02233ec4cb14b8ac4
gawlind <gawlind@wwu.edu>
2019-02-16 12:30:21 -0800

    Change Project Direction

---
+++ Client/src/hapless/eagles/client/Main.java
@@ -0,0 +1,12 @@
+package hapless.eagles.client;
+
+/**
+ * The main client class.
+ * Created by Kneesnap on 2/16/19.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
---
+++ Server/src/hapless/eagles/server/Main.java
@@ -0,0 +1,11 @@
+package hapless.eagles.server;
+
+/**
+ * The main server class.
+ * Created by Kneesnap on 2/16/19.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
