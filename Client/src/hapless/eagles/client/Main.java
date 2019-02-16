e4a0d8d80c5aa72b1518ea1d9dd5d837212e6d41
Kneesnap <davidmenlo2@gmail.com>
2019-02-16 13:48:27 -0800

    Create basic world configuration to load.

--- Client/src/hapless/eagles/client/Main.java
+++ Client/src/hapless/eagles/client/Main.java
@@ -1,12 +1,21 @@
 package hapless.eagles.client;
 
+import hapless.eagles.common.World;
+import hapless.eagles.common.utils.Config;
+
+import java.io.File;
+
 /**
  * The main client class.
  * Created by Kneesnap on 2/16/19.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world.");
+    public static void main(String[] args) throws Exception {
+        System.out.println("Client Start");
+
+        World world = new World();
+        world.load(new Config(new File("debug.cfg")));
+        System.out.println("Loaded World Config.");
     }
 }
